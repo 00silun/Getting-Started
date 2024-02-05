d98c60be1bf306eb43e5e5876c17213db5e12c89
Simon Lundgren <simon00.lundgren@gmail.com>
2024-02-05 17:13:24 +0100

    Initial Commit

---
+++ Getting Started/Getting_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getting_StartedApp.swift
+//  Getting Started
+//
+//  Created by Simon Lundgren on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Getting_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
